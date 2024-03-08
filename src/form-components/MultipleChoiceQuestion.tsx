c283844e93a2b9645caaf2440134768c799093d0
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-03-08 16:03:24 -0500

    finished implementing all form component functionality

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState(options[0]);
+    const radioButtons = options.map((option: string) => {
+        return (
+            <Form.Check
+                inline
+                type="radio"
+                key={option}
+                name={option}
+                onChange={(e) => setUserAnswer(e.target.value)}
+                label={option}
+                value={option}
+                checked={userAnswer === option}
+            ></Form.Check>
+        );
+    });
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <div>{radioButtons}</div>
+            <div>
+                <span>{expectedAnswer === userAnswer ? "✔️" : "❌"}</span>
+            </div>
         </div>
     );
 }
