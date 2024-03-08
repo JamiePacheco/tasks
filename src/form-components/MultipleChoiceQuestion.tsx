2fe621b3b1b00fc57cffc0f5c24ce8862418094e
Jamie <jamie.pacheco.71@gmail.com>
2024-03-08 16:44:07 -0500

    changed radio buttons to drop down in MultipleChoiceQuestion.tsx

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -9,24 +9,26 @@ export function MultipleChoiceQuestion({
     expectedAnswer: string;
 }): JSX.Element {
     const [userAnswer, setUserAnswer] = useState(options[0]);
-    const radioButtons = options.map((option: string) => {
+    const mappedOptions = options.map((option: string) => {
         return (
-            <Form.Check
-                inline
-                type="radio"
-                key={option}
-                name={option}
-                onChange={(e) => setUserAnswer(e.target.value)}
-                label={option}
-                value={option}
-                checked={userAnswer === option}
-            ></Form.Check>
+            <option key={option} value={option}>
+                {option}
+            </option>
         );
     });
     return (
         <div>
             <h3>Multiple Choice Question</h3>
-            <div>{radioButtons}</div>
+            <div>
+                <Form.Group controlId="choices">
+                    <Form.Select
+                        value={userAnswer}
+                        onChange={(e) => setUserAnswer(e.target.value)}
+                    >
+                        {mappedOptions}
+                    </Form.Select>
+                </Form.Group>
+            </div>
             <div>
                 <span>{expectedAnswer === userAnswer ? "✔️" : "❌"}</span>
             </div>
