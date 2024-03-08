2b7fa1f86651d259e3ba3e44ff9487c870ee5b1a
Jamie <jamie.pacheco.71@gmail.com>
2024-03-08 00:37:50 -0500

    finished implementing functionality for most of the components

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,37 @@
+import { Console } from "console";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentAnswer, setCurrentAnswer] = useState<string>("");
+
+    function updateCurrentAnswer(e: React.ChangeEvent<HTMLInputElement>) {
+        setCurrentAnswer(e.target.value);
+        console.log(currentAnswer);
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <div>
+                <Form.Group controlId="userAnswer">
+                    <Form.Label>Answer</Form.Label>
+                    <Form.Control
+                        value={currentAnswer}
+                        onChange={updateCurrentAnswer}
+                    ></Form.Control>
+                </Form.Group>
+            </div>
+            <div>
+                {currentAnswer === expectedAnswer ? (
+                    <span>✔️</span>
+                ) : (
+                    <span>❌</span>
+                )}
+            </div>
         </div>
     );
 }
